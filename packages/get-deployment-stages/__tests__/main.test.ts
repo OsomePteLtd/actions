b04861a9243986a4fcbf675be5f982d899de406b
Egor Zaitsev <heuels@osome.com>
2020-07-28 17:58:33 +0300

    Support production env for workflow_dispatch events and change output format (#3)

--- packages/get-deployment-stages/__tests__/main.test.ts
+++ packages/get-deployment-stages/__tests__/main.test.ts
@@ -1,54 +1,150 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { promises as fs } from "fs";
+import { promises as fs } from 'fs';
 
 import run from '../';
 
-const refMaster = 'refs/heads/master';
+jest.mock('@actions/core');
+jest.mock('@actions/github');
+jest.mock('fs', () => ({ promises: { readFile: jest.fn() } }));
+const mockReadFile = fs.readFile as jest.Mock;
 
 describe('get-deployment-stages', () => {
-  it('should return the stage for the master branch', async () => {
-    process.env['GITHUB_EVENT_PATH'] = 'test';
-    fs.readFile = jest.fn().mockResolvedValue(JSON.stringify({ test: 'data' }));
-    const setFailedMock = jest.spyOn(core, 'setFailed');
-
-    github.context.ref = refMaster;
-
-    jest.spyOn(core, 'setOutput').mockImplementation((key: string, value: string) => {
-      expect(key).toEqual('stages');
-      expect(value).toEqual("[\"stage\"]");
-    });
-
-    await expect(run()).resolves.not.toThrow();
-
-    expect(fs.readFile).toBeCalledTimes(1);
-    expect(setFailedMock).toBeCalledTimes(0);
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
-  it('should return a branch name as default way', async () => {
-    process.env['GITHUB_EVENT_PATH'] = 'test';
-    const event = {
-      pull_request: {
-        head: {
-          ref: 'feature/branch-name',
-        },
-        labels: [],
-      },
-    };
-    fs.readFile = jest.fn().mockResolvedValue(JSON.stringify(event));
-    const setFailedMock = jest.spyOn(core, 'setFailed');
+  describe('push', () => {
+    it('sets output to stage when ref is master', async () => {
+      const event = {};
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'push';
+      github.context.ref = 'refs/heads/master';
 
-    github.context.ref = 'test';
-    github.context.eventName = 'pull_request';
+      await expect(run()).resolves.not.toThrow();
 
-    jest.spyOn(core, 'setOutput').mockImplementation((key: string, value: string) => {
-      expect(key).toEqual('stages');
-      expect(value).toEqual("[\"feature-branch-name\"]");
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"stage\\",\\"transient_environment\\":true,\\"production_environment\\":false}]"`,
+      );
+    });
+  });
+
+  describe('pull_request', () => {
+    it('sets output to branch name when env is not provided', async () => {
+      const event = {
+        pull_request: { head: { ref: 'feature/test' }, labels: [] },
+      };
+
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'pull_request';
+      github.context.ref = 'refs/heads/feature/test';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"feature-test\\",\\"transient_environment\\":false,\\"production_environment\\":false}]"`,
+      );
     });
 
-    await expect(run()).resolves.not.toThrow();
+    it('sets output to branch name when env is production', async () => {
+      const event = {
+        pull_request: { head: { ref: 'feature/test' }, labels: [{ name: '' }] },
+      };
 
-    expect(fs.readFile).toBeCalledTimes(1);
-    expect(setFailedMock).toBeCalledTimes(0);
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'pull_request';
+      github.context.ref = 'refs/heads/feature/test';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"feature-test\\",\\"transient_environment\\":false,\\"production_environment\\":false}]"`,
+      );
+    });
+
+    it('sets output when multiple envs are provided', async () => {
+      const event = {
+        pull_request: {
+          head: { ref: 'feature/test' },
+          labels: [{ name: 'deployed to test-1' }, { name: 'deployed to test-2' }],
+        },
+      };
+
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'pull_request';
+      github.context.ref = 'refs/heads/feature/test';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"test-1\\",\\"transient_environment\\":true,\\"production_environment\\":false},{\\"name\\":\\"test-2\\",\\"transient_environment\\":true,\\"production_environment\\":false}]"`,
+      );
+    });
+  });
+
+  describe('workflow_event', () => {
+    it('sets failed when env is production and ref is not master', async () => {
+      const event = { inputs: { environment: 'production' } };
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'workflow_dispatch';
+      github.context.ref = 'refs/heads/feature/test';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(1);
+      expect(setOutput).toHaveBeenCalledTimes(0);
+      expect(setFailed.mock.calls[0][0]).toMatchInlineSnapshot(`"Can deploy to production from master branch only"`);
+    });
+
+    it('sets output when env is production and ref is master', async () => {
+      const event = { inputs: { environment: 'production' } };
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'workflow_dispatch';
+      github.context.ref = 'refs/heads/master';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"production\\",\\"transient_environment\\":true,\\"production_environment\\":true}]"`,
+      );
+    });
+
+    it('sets output when env is stage and ref is not master', async () => {
+      const event = { inputs: { environment: 'stage' } };
+      mockReadFile.mockResolvedValue(JSON.stringify(event));
+      github.context.eventName = 'workflow_dispatch';
+      github.context.ref = 'refs/heads/feature/test';
+
+      await expect(run()).resolves.not.toThrow();
+
+      const { setFailed, setOutput } = core as jest.Mocked<typeof core>;
+      expect(setFailed).toHaveBeenCalledTimes(0);
+      expect(setOutput).toHaveBeenCalledTimes(1);
+      expect(setOutput.mock.calls[0][0]).toBe(`stages`);
+      expect(setOutput.mock.calls[0][1]).toMatchInlineSnapshot(
+        `"[{\\"name\\":\\"stage\\",\\"transient_environment\\":true,\\"production_environment\\":false}]"`,
+      );
+    });
   });
 });
--- packages/get-deployment-stages/index.ts
+++ packages/get-deployment-stages/index.ts
@@ -1,20 +1,46 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { Webhooks } from "@octokit/webhooks";
-import { promises as fs } from "fs";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Webhooks } from '@octokit/webhooks';
+import { promises as fs } from 'fs';
+
+interface Stage {
+  name: string;
+  transient_environment: boolean;
+  production_environment: boolean;
+}
 
 const STAGE_LABELS: Record<string, string> = {
-  "deployed to stage": "stage",
-  "deployed to test-1": "test-1",
-  "deployed to test-2": "test-2",
-  "deployed to test-3": "test-3",
-  "deployed to test-4": "test-4",
-  "deployed to test-5": "test-5",
-  "deployed to test-6": "test-6",
-  "deployed to test-7": "test-7",
-  "deployed to test-8": "test-8",
-  "deployed to test-9": "test-9",
+  '': 'production',
+  'deployed to stage': 'stage',
+  'deployed to test-1': 'test-1',
+  'deployed to test-2': 'test-2',
+  'deployed to test-3': 'test-3',
+  'deployed to test-4': 'test-4',
+  'deployed to test-5': 'test-5',
+  'deployed to test-6': 'test-6',
+  'deployed to test-7': 'test-7',
+  'deployed to test-8': 'test-8',
+  'deployed to test-9': 'test-9',
 };
+
+const isMaster = (ref: string) => ref === 'refs/heads/master';
+const isProductionEnv = (env: string) => env === 'production';
+const isTransientEnv = (env: string) => Object.values(STAGE_LABELS).includes(env);
+
+const toStages = (envs: string[]) =>
+  JSON.stringify(
+    envs.reduce(
+      (envs, env) => [
+        ...envs,
+        {
+          name: env,
+          transient_environment: isTransientEnv(env),
+          production_environment: isProductionEnv(env),
+        },
+      ],
+      [] as Stage[],
+    ),
+  );
 
 async function run() {
   try {
@@ -24,14 +50,31 @@ async function run() {
 
     const { eventName, ref } = github.context;
 
+    if (eventName === 'workflow_dispatch') {
+      // There are currently no types present for
+      // workflow_dispatch in @octokit/webhooks package.
+      const environment = (event as any).inputs.environment;
+      const stages = Object.values(STAGE_LABELS).filter((label) => label === environment);
+
+      if (!isMaster(ref) && isProductionEnv(environment)) {
+        throw new Error('Can deploy to production from master branch only');
+      }
+
+      if (stages.length) {
+        core.setOutput('stages', toStages(stages));
+      }
+
+      return;
+    }
+
     // Workflows run on master branch should only deploy to stage.
-    if (ref === "refs/heads/master") {
-      core.setOutput("stages", JSON.stringify(['stage']));
+    if (isMaster(ref)) {
+      core.setOutput('stages', toStages(['stage']));
       return;
     }
 
     // Workflows run on pull request can deploy anywhere else.
-    if (eventName === "pull_request") {
+    if (eventName === 'pull_request') {
       const {
         head: { ref: branchName },
         labels,
@@ -39,18 +82,18 @@ async function run() {
 
       const stages = labels
         .map(({ name: labelName }) => STAGE_LABELS[labelName])
-        .filter(Boolean);
+        .filter((stage) => !!stage && !isProductionEnv(stage));
 
       // If stage-specific labels are found, use them.
       if (stages.length) {
-        core.setOutput("stages", JSON.stringify(stages));
+        core.setOutput('stages', toStages(stages));
         return;
       }
 
       // If no stage-specific labels are found, use task name or branch name.
       if (!stages.length) {
         const output = [branchName.replace(/[\/|=]/, '-').toLowerCase()];
-        core.setOutput("stages", JSON.stringify(output));
+        core.setOutput('stages', toStages(output));
         return;
       }
     }
@@ -61,7 +104,7 @@ async function run() {
 
 // Don't auto-execute in the test environment
 if (process.env['NODE_ENV'] !== 'test') {
-  run()
+  run();
 }
 
 export default run;
