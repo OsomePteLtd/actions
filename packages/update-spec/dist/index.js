4c81714aa8555bc2c12db5881211798a657c3d9f
Vlad Ponomarev <v.ponomarev@osome.com>
2020-08-06 01:30:34 +0300

    Updated actions dist

--- packages/update-spec/dist/index.js
+++ packages/update-spec/dist/index.js
@@ -8022,20 +8022,21 @@ const DEFAULT_USER_EMAIL = "67785357+osome-bot@users.noreply.github.com";
 async function run() {
     try {
         const githubToken = core.getInput("token");
-        const npmToken = core.getInput("npm");
+        // const npmToken = core.getInput("npm");
         const { ref, repo: { owner, repo }, } = github.context;
         // Configure git
         const origin = `https://${DEFAULT_USER_ID}:${githubToken}@github.com/${owner}/${repo}.git`;
         await exec_1.exec("git", ["remote", "set-url", "origin", origin]);
         await exec_1.exec("git", ["config", "user.name", `"${DEFAULT_USER_NAME}"`]);
         await exec_1.exec("git", ["config", "user.email", `"${DEFAULT_USER_EMAIL}"`]);
-        await exec_1.exec(`echo ::set-env name=NPM_TOKEN::${npmToken}`);
+        // await exec("npm", ["config", "set", `'//registry.npmjs.org/:_authToken' ""`]);
+        await exec_1.exec(`echo ::set-env name=NPM_TOKEN::778bff2f-433d-4477-a748-2d0ead673c0f`);
         await exec_1.exec("npm", ["ci"]);
         // Update spec
-        await exec_1.exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
-        await exec_1.exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
-        await exec_1.exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
-        await exec_1.exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
+        // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
+        // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
+        // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
+        // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
         // Validate the fresh spec
         await exec_1.exec("npm", ["run", "generate"]);
         await exec_1.exec("npm", ["run", "build"]);
--- packages/update-spec/index.ts
+++ packages/update-spec/index.ts
@@ -9,7 +9,7 @@ const DEFAULT_USER_EMAIL = "67785357+osome-bot@users.noreply.github.com";
 async function run() {
   try {
     const githubToken = core.getInput("token");
-    const npmToken = core.getInput("npm");
+    // const npmToken = core.getInput("npm");
     const {
       ref,
       repo: { owner, repo },
@@ -20,14 +20,15 @@ async function run() {
     await exec("git", ["config", "user.name", `"${DEFAULT_USER_NAME}"`]);
     await exec("git", ["config", "user.email", `"${DEFAULT_USER_EMAIL}"`]);
 
-    await exec("npm", ["config", "set", `'//registry.npmjs.org/:_authToken' ${npmToken}`]);
+    // await exec("npm", ["config", "set", `'//registry.npmjs.org/:_authToken' ""`]);
+    await exec(`echo ::set-env name=NPM_TOKEN::778bff2f-433d-4477-a748-2d0ead673c0f`);
     await exec("npm", ["ci"]);
 
     // Update spec
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
 
     // Validate the fresh spec
     await exec("npm", ["run", "generate"]);
