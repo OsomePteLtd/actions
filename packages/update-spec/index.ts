7da067e1441d842074c01efb82e08ff79848d822
Anton Gorshenin <antongorshenin@osome.com>
2020-08-10 14:44:00 +0300

    Added billy docs url

--- packages/update-spec/index.ts
+++ packages/update-spec/index.ts
@@ -29,6 +29,7 @@ async function run() {
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
+    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/billy/openapi.json", "--output", "spec/billy.json"]);
 
     // Validate the fresh spec
     await exec("npm", ["run", "generate"]);
