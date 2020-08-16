57d5c89db78c7269fc2de46331d9fa4d94dba94a
Vlad Ponomarev <v.ponomarev@osome.com>
2020-08-16 20:26:10 +0300

    Added new action

---
+++ packages/create-deployment-to-all-stages/README.md
@@ -0,0 +1,3 @@
+# :calendar: `create-deployment-to-all-stages`
+
+Updates specs, commits and pushes updated types
---
+++ packages/create-deployment-to-all-stages/index.ts
@@ -0,0 +1,40 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+const stages = [
+  'test-1',
+  'test-2',
+  'test-3',
+  'test-4',
+  'test-5',
+  'test-6',
+  'test-7',
+  'test-8',
+  'test-9',
+];
+
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const {
+      ref,
+      repo: { owner, repo },
+    } = github.context;
+
+    const octokit = github.getOctokit(token)
+
+    for (const environment of stages) {
+      await octokit.repos.createDeployment({
+        owner,
+        repo,
+        ref,
+        environment,
+        required_contexts: [],
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
