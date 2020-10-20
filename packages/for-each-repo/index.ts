845493a8b31a0debb93185480e4f7ed751885573
Egor Zaitsev <heuels@gmail.com>
2020-10-20 22:25:47 +0300

    wip

--- packages/for-each-repo/index.ts
+++ packages/for-each-repo/index.ts
@@ -3,15 +3,14 @@ import * as github from '@actions/github';
 
 async function run() {
   try {
-    const org = core.getInput('org') ?? 'OsomePteLtd';
+    const org = core.getInput('org') || 'OsomePteLtd';
     const token = core.getInput('token', { required: true });
     const octokit = github.getOctokit(token);
-
-    const repos = await octokit.repos.listForOrg({ org });
-    return JSON.stringify(repos.data.map((repo) => ({ owner: repo.owner, repo: repo.name })));
+    const repos = await octokit.repos.listForOrg({ org, page: 1, per_page: 100 });
+    core.setOutput('repos', JSON.stringify(repos.data.map((repo) => ({ owner: repo.owner.login, repo: repo.name }))));
   } catch (error) {
+    console.error(error);
     core.setFailed(error.message);
-    return JSON.stringify([]);
   }
 }
 
