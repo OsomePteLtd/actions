082f600f9d48b551d06a3799b0ac110b65679e3c
Vlad Ponomarev <v.ponomarev@osome.com>
2020-12-06 21:44:10 +0300

    Download branch from e2e

--- packages/run-e2e/src/index.ts
+++ packages/run-e2e/src/index.ts
@@ -18,19 +18,27 @@ async function downloadE2ERepo(path: string) {
   const token = core.getInput('token', { required: true });
   const octokit = github.getOctokit(token);
 
-  console.log({ ref });
-  const temp = await octokit.repos.getBranch({
-    owner,
-    repo: e2eRepo,
-    branch: ref,
-  });
-
-  console.log({ temp });
-  const downloadedRepo = await octokit.repos.downloadZipballArchive({
-    owner,
-    repo: e2eRepo,
-    ref: 'master',
-  });
+  let downloadedRepo = null;
+  try {
+    await octokit.repos.getBranch({
+      owner,
+      repo: e2eRepo,
+      branch: ref,
+    });
+    console.log(`Using branch ${ref} for tests`);
+    downloadedRepo = await octokit.repos.downloadZipballArchive({
+      owner,
+      repo: e2eRepo,
+      ref: 'master',
+    });
+  } catch (e) {
+    console.log(`Can't branch ${ref} in https://github.com/OsomePteLtd/e2e, using master`);
+    downloadedRepo = await octokit.repos.downloadZipballArchive({
+      owner,
+      repo: e2eRepo,
+      ref: 'master',
+    });
+  }
 
   const zip = new AdmZip(Buffer.from(downloadedRepo.data as ArrayBuffer));
   zip.extractAllTo(path, true);
