755f70ef9cdabe9578649a7247a79ad79dc195b8
Vlad Ponomarev <v.ponomarev@osome.com>
2020-12-06 21:01:49 +0300

    Download e2e repo instead of current

--- packages/run-e2e/src/index.ts
+++ packages/run-e2e/src/index.ts
@@ -6,21 +6,21 @@ import { exec } from "@actions/exec";
 async function run() {
   const {
     ref,
-    repo: { owner, repo },
+    repo: { owner },
   } = github.context;
   const token = core.getInput('token', { required: true });
   const octokit = github.getOctokit(token);
 
   const downloadUrl = await octokit.repos.downloadZipballArchive({
     owner,
-    repo,
+    repo: 'e2e',
     ref,
   });
 
   const zip = new AdmZip(Buffer.from((downloadUrl.data as ArrayBuffer)));
   zip.extractAllTo('./e2e',true);
 
-  await exec('ls -R');
+  await exec('cd ./e2e && ls -R');
 
   console.log({ downloadUrl });
 }
