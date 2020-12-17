5ac07b15f3dd892095ccf861f246567e6969de2e
Egor Zaitsev <heuels@gmail.com>
2020-12-17 14:45:27 +0300

    fix(bump-version): push only latest tag

--- packages/bump-version/index.ts
+++ packages/bump-version/index.ts
@@ -1,14 +1,14 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { exec } from "@actions/exec";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { exec } from '@actions/exec';
 
-const DEFAULT_USER_ID = "osome-bot";
-const DEFAULT_USER_NAME = "Osome Bot";
-const DEFAULT_USER_EMAIL = "67785357+osome-bot@users.noreply.github.com";
+const DEFAULT_USER_ID = 'osome-bot';
+const DEFAULT_USER_NAME = 'Osome Bot';
+const DEFAULT_USER_EMAIL = '67785357+osome-bot@users.noreply.github.com';
 
 async function run() {
   try {
-    const token = core.getInput("token");
+    const token = core.getInput('token');
     const {
       ref,
       repo: { owner, repo },
@@ -16,13 +16,22 @@ async function run() {
 
     // Configure git
     const origin = `https://${DEFAULT_USER_ID}:${token}@github.com/${owner}/${repo}.git`;
-    await exec("git", ["remote", "set-url", "origin", origin]);
-    await exec("git", ["config", "user.name", `"${DEFAULT_USER_NAME}"`]);
-    await exec("git", ["config", "user.email", `"${DEFAULT_USER_EMAIL}"`]);
+    await exec('git', ['remote', 'set-url', 'origin', origin]);
+    await exec('git', ['config', 'user.name', `"${DEFAULT_USER_NAME}"`]);
+    await exec('git', ['config', 'user.email', `"${DEFAULT_USER_EMAIL}"`]);
 
     // Bump version and push the commit and tag
-    await exec("npm", ["version", "minor"]);
-    await exec("git", ["push", "origin", `HEAD:${ref}`, "--tags"]);
+    let tag = '';
+    await exec('npm', ['version', 'minor'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          tag += data.toString();
+        },
+      },
+    });
+
+    await exec('git', ['push', 'origin', `HEAD:${ref}`]);
+    await exec('git', ['push', 'origin', `refs/tags/${tag}`]);
   } catch (error) {
     core.setFailed(error.message);
   }
