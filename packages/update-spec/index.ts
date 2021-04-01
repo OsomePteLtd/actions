6d834cebdc3cd740c117210dd4670399a3af9bcc
Dmitry Lyapin <dma@cranbee.com>
2021-04-01 07:46:30 +0300

    IN-89: Move Pablo spec to SDK

--- packages/update-spec/index.ts
+++ packages/update-spec/index.ts
@@ -27,7 +27,7 @@ async function run() {
     // Update spec
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/billy/openapi.json", "--output", "spec/billy.json"]);
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pechkin/openapi.json", "--output", "spec/pechkin.json"]);
@@ -36,6 +36,7 @@ async function run() {
     await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/payot/openapi.json", "--output", "spec/payot.json"]);
 
     // Validate the fresh spec
+    await exec("npm", ["run", "spec"]);
     await exec("npm", ["run", "generate"]);
     await exec("npm", ["run", "build"]);
 
