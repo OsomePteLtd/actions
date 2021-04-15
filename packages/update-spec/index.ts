fa9487052dfeceda8884558444624868a65d8962
Dmitry Lyapin <dmlyapin@osome.com>
2021-04-15 12:59:20 +0300

    IN-98: Move Core Spec to SDK (#66)

--- packages/update-spec/index.ts
+++ packages/update-spec/index.ts
@@ -25,8 +25,8 @@ async function run() {
     await exec("npm", ["ci"]);
 
     // Update spec
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
-    await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/agent/openapi.json", "--output", "spec/core.json"]);
+    // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/seed/openapi.json", "--output", "spec/seed.json"]);
     // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/pablo/openapi.json", "--output", "spec/pablo.json"]);
     // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/hero/openapi.json", "--output", "spec/hero.json"]);
     // await exec("curl", ["-f", "-u", "ooo:some!", "https://docs.osome.club/api/stage/billy/openapi.json", "--output", "spec/billy.json"]);
