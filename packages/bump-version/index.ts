b8f0f49469e86579b99860bf985b067949171a6c
Aleksey Leshko <aleksey.leshko@osome.com>
2021-10-20 17:43:17 +0800

    Add the working-directory parameter (#104)

--- packages/bump-version/index.ts
+++ packages/bump-version/index.ts
@@ -9,6 +9,7 @@ const DEFAULT_USER_EMAIL = '67785357+osome-bot@users.noreply.github.com';
 async function run() {
   try {
     const token = core.getInput('token');
+    const workingDirectory = core.getInput('working-directory', { required: false });
     const {
       ref,
       repo: { owner, repo },
@@ -20,15 +21,22 @@ async function run() {
     await exec('git', ['config', 'user.name', `"${DEFAULT_USER_NAME}"`]);
     await exec('git', ['config', 'user.email', `"${DEFAULT_USER_EMAIL}"`]);
 
+    // Why create the directory? https://github.com/npm/npm/issues/9111#issuecomment-126279242
+    if (workingDirectory) {
+      await exec('mkdir', ['.git'], { cwd: workingDirectory });
+    }
+
     // Bump version and push the commit and tag
     let tag = '';
-    await exec('npm', ['version', 'minor'], {
+    await exec('npm', ['version', 'patch'], {
       listeners: {
         stdout: (data: Buffer) => {
           tag += data.toString();
         },
       },
+      cwd: workingDirectory,
     });
+
 
     await exec('git', ['push', 'origin', `HEAD:${ref}`]);
     await exec('git', ['push', 'origin', `refs/tags/${tag.trim()}`]);
