5a5c577cd9a8999d75b89f073a1f72fcd85693bf
Denis Panferov <denis.panferov@osome.com>
2023-09-14 16:24:22 +0300

    update lokalise download action

--- packages/lokalise-download/download.sh
+++ packages/lokalise-download/download.sh
@@ -1,5 +1,21 @@
 set -ex
 
+echo " Going to start lokalise download with the folloeing args:\n
+        project-id -> $2
+        unzip-to -> $3 
+        format -> $4
+        bundle-structure -> $5
+        placeholder-format -> $6
+        triggers -> $7
+        add_newline_eof -> $8
+        filter_repositories -> $9 
+        filter_filenames -> ${10}
+        plural_format -> ${11}
+        indentation -> ${12}
+        export_sort -> ${13}
+        original-filenames -> ${14}  
+     "
+
 lokalise2 \
     --token $1 \
     --project-id $2 \
