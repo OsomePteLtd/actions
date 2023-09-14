2893bd3944ac4933f2f8b2932672e5776bbd4425
Denis Panferov <102283882+mrden1@users.noreply.github.com>
2023-09-14 18:30:59 +0300

    Feature/pl 2204 add lokalise download action (#252)
    * add lokalise download action
    
    * format
    
    * update lokalise download action
    
    * format
    
    * format
    
    * update lokalise download action
    
    * update lokalise download action
    
    * update permission for script
    
    * update lokalise download action
    
    * update lokalise download action
    
    * update lokalise download action
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * add directory-prefix arg
    
    * add lang iso to directory-prefix arg
    
    * add description for directory-prefix arg

---
+++ packages/lokalise-download/download.sh
@@ -0,0 +1,36 @@
+set -e
+
+echo " Going to start lokalise download with the folloeing args:\n
+        project-id -> $2
+        unzip-to -> $3 
+        format -> $4
+        bundle-structure -> $5
+        placeholder-format -> $6
+        triggers -> $7
+        add-newline-eof -> $8
+        filter-repositories -> $9 
+        filter-filenames -> ${10}
+        plural-format -> ${11}
+        indentation -> ${12}
+        export-sort -> ${13}
+        original-filenames -> ${14}
+        directory-prefix -> ${15}
+     "
+
+lokalise2 \
+    --token $1 \
+    --project-id $2 \
+    file download \
+    --format $4 \
+    --unzip-to $3 \
+    --bundle-structure $5 \
+    --placeholder-format $6 \
+    --triggers $7 \
+    --add-newline-eof $8 \
+    --filter-repositories $9 \
+    --filter-filenames ${10} \
+    --plural-format ${11} \
+    --indentation ${12} \
+    --export-sort ${13} \
+    --original-filenames ${14} \
+    --directory-prefix ${15}
