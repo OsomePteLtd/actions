ccaf3a5499d3331101459f823e5414f7c257512d
Artem <Rendalf@users.noreply.github.com>
2023-09-18 09:59:01 +0200

    Lokalise download: unescape slashes in json (#254)

--- packages/lokalise-download/download.sh
+++ packages/lokalise-download/download.sh
@@ -2,19 +2,20 @@ set -e
 
 echo " Going to start lokalise download with the folloeing args:\n
         project-id -> $2
-        unzip-to -> $3 
+        unzip-to -> $3
         format -> $4
         bundle-structure -> $5
         placeholder-format -> $6
         triggers -> $7
         add-newline-eof -> $8
-        filter-repositories -> $9 
+        filter-repositories -> $9
         filter-filenames -> ${10}
         plural-format -> ${11}
         indentation -> ${12}
         export-sort -> ${13}
         original-filenames -> ${14}
         directory-prefix -> ${15}
+        json-unescaped-slashes -> ${16}
      "
 
 lokalise2 \
@@ -33,4 +34,5 @@ lokalise2 \
     --indentation ${12} \
     --export-sort ${13} \
     --original-filenames ${14} \
-    --directory-prefix ${15}
+    --directory-prefix ${15} \
+    --json-unescaped-slashes ${16}
