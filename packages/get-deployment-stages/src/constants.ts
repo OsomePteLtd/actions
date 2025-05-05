3f40e6355eacc4d25d0feca5895f1d7c55df7d40
AlekseyLeshko <aleksey.leshko@osome.com>
2025-05-05 19:51:47 +0700

    Test envs: use only first 3 circuits

--- packages/get-deployment-stages/src/constants.ts
+++ packages/get-deployment-stages/src/constants.ts
@@ -4,12 +4,6 @@ export const STAGE_LABELS: Record<string, string> = {
   'deployed to test-1': 'test-1',
   'deployed to test-2': 'test-2',
   'deployed to test-3': 'test-3',
-  'deployed to test-4': 'test-4',
-  'deployed to test-5': 'test-5',
-  'deployed to test-6': 'test-6',
-  'deployed to test-7': 'test-7',
-  'deployed to test-8': 'test-8',
-  'deployed to test-9': 'test-9',
 };
 
-export const previews = ["ant-man", "flash"];
+export const previews = ['ant-man', 'flash'];
